355097bfb9603aa99fd81be635aeec4eeb93b00e
ben14 <544652471@qq.com>
2019-11-27 10:24:26 +0800

    feat: init .

---
+++ src/components/editor.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useRef } from "react";
+import EditorJS from "@editorjs/editorjs";
+import Header from "@editorjs/header";
+
+export default function Editor() {
+  const codexEditorEl = useRef<any>(null);
+  useEffect(() => {
+    const editor = new EditorJS({
+      holder: codexEditorEl.current,
+      placeholder: "Let`s write an awesome story!",
+      tools: {
+        header: {
+          class: Header,
+          inlineToolbar: true
+        }
+      },
+      onChange: () => {
+        editor
+          .save()
+          .then(outputData => {
+            console.log("Article data: ", outputData);
+          })
+          .catch(error => {
+            console.log("Saving failed: ", error);
+          });
+        console.log("Now I know that Editor's content changed!");
+      }
+    });
+  }, []);
+  return (
+    <div className="editor">
+      <div className="codex-editor" ref={codexEditorEl}></div>
+    </div>
+  );
+}
